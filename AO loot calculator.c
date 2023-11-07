3bfeb2d0edfac02248a9da6404b9df36242f7f21
KemalYurtsever <117767528+KemalYurtsever@users.noreply.github.com>
2023-11-07 23:49:02 +0200

    Add files via upload

---
+++ AO loot calculator.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+    float gank_loot, repair, vergi, split;
+    int people;
+    
+    printf("Gank lootu gir: ");
+    scanf("%f", &gank_loot);
+    
+    printf("Repairlarý gir (sonuncu repairi girdikten sonra 0 enter bas): ");
+    
+    repair = 0;
+    while (1) {
+        float input;
+        scanf("%f", &input);
+
+        if (input == 0) {
+            break;
+        }
+
+        repair += input;
+    }
+
+    printf("Kaç kiþi: ");
+    scanf("%d", &people);
+    
+    vergi = gank_loot * 12 / 100;
+    split = (gank_loot - vergi - repair) / people;
+    
+    printf("VERILECEK SPLIT= %.2f", split);
+
+    return 0;
+}
+
+	
